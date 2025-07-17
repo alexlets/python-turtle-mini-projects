8fcdd1c8f7ed6e42d818b826f1d8b085ac201a41
alexlets <alexandros.letsios99@gmail.com>
2025-07-17 21:31:23 +0300

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,20 @@
+import random
+import turtle as t
+import random as r
+
+tim = t.Turtle()
+
+t.colormode(255)
+
+for sides in range(3,11):
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    tim.color(r,g,b)
+    angle = 360 / sides
+    # print(f"Sides: {sides}, Angle: {angle}")
+    for i in range(sides):
+        tim.forward(100)
+        tim.left(angle)
+
+t.exitonclick()
